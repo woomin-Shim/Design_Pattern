a84b7edf7318f289acde6c7db0533171a67634ad
Woomin <worejj@gmail.com>
2021-03-22 18:45:35 +0900

    Factory

---
+++ Factory/idcard/IDCard.java
@@ -0,0 +1,17 @@
+package idcard;
+import framework.*;  //connect package
+
+public class IDCard extends Product{
+	private String owner;
+	IDCard(String owner) {
+		this.owner=owner;
+		System.out.println(owner + "의 카드를 만듭니다.");
+		
+	}
+	public void use() {
+		System.out.println(owner + "의 카드를 사용합니다.");
+	}
+	public String getOwner() {
+		return owner;
+	}
+}
